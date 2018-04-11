d7e7dbff83f3caa7b27e588fb835cf9622f57948
Will Mann <william@mannclan.net>
2018-04-11 11:47:51 +0100

    Added Index file for Azure.

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ var verifyTokenApp = require("./auth").verifyTokenApp;
 //var uri = 'mongodb://ridge-mongodb:jL74RbgxKAcBoQx8xChqtcQmUkR0ecixULdp8sfH4xpdkU4TXAkpyjk3DxqHDmE3Iby6HhaCCOOH5grAWFIQmw==@ridge-mongodb.documents.azure.com:10255/ridge-wellington?ssl=true&sslverifycertificate=false';
 var uri = "mongodb://ridge-wellington:qcyok9bI1fZVH8Y7lpTWH1HKbqQhKnMuq4eEbt7GCOLUCiHN75lCaKplHs43wBU9UusSkOgMX8uAca3ny6tVsA==@ridge-wellington.documents.azure.com:10255/wellington?ssl=true&replicaSet=globaldb";
 //var uri = 'mongodb://RIDGE:C0d1ngG33k@ridgewelly-shard-00-00-lamqk.mongodb.net:27017,ridgewelly-shard-00-01-lamqk.mongodb.net:27017,ridgewelly-shard-00-02-lamqk.mongodb.net:27017/test?ssl=true&replicaSet=RIDGEWelly-shard-0&authSource=admin'
-var port = 8080;
+var port = 80;
 var mongoose = require("mongoose");
 mongoose.connect(uri);
 var db = mongoose.connection;
